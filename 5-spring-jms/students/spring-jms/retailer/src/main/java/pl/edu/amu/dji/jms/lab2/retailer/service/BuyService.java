a701bb519f35573afece0770536756445a49f7f7
Łukasz P <lukpawlucz@gmail.com>
2014-11-06 09:41:04 +0100

    ex 5

--- 5-spring-jms/students/spring-jms/retailer/src/main/java/pl/edu/amu/dji/jms/lab2/retailer/service/BuyService.java
+++ 5-spring-jms/students/spring-jms/retailer/src/main/java/pl/edu/amu/dji/jms/lab2/retailer/service/BuyService.java
@@ -1,9 +1,15 @@
 package pl.edu.amu.dji.jms.lab2.retailer.service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import org.springframework.jms.core.JmsTemplate;
 
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.Session;
+import org.springframework.jms.core.MessageCreator;
 
 public class BuyService implements MessageListener {
 
@@ -21,6 +27,22 @@ public class BuyService implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
-        throw new UnsupportedOperationException();
+        try {
+            MapMessage mapMessage = (MapMessage) message;
+            double price = mapMessage.getDouble("price");
+            if (maxPrice.compareTo(price) == 1) {
+                jmsTemplate.send(mapMessage.getJMSReplyTo(), new MessageCreator() {
+
+                    public Message createMessage(Session sn) throws JMSException {
+                        MapMessage msg = sn.createMapMessage();
+                        msg.setString("retailerID", getClass().getName());
+                        msg.setDouble("quantity", 3.0);
+                        return msg;
+                    }
+                });
+            }
+        } catch (JMSException ex) {
+            Logger.getLogger(BuyService.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
