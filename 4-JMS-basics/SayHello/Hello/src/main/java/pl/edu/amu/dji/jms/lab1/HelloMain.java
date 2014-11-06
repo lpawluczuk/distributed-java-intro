bcc119d810320098b476702086aeaa056ed81507
Łukasz P <lukpawlucz@gmail.com>
2014-11-06 08:56:49 +0100

    ex 4

--- 4-JMS-basics/SayHello/Hello/src/main/java/pl/edu/amu/dji/jms/lab1/HelloMain.java
+++ 4-JMS-basics/SayHello/Hello/src/main/java/pl/edu/amu/dji/jms/lab1/HelloMain.java
@@ -1,50 +1,60 @@
 package pl.edu.amu.dji.jms.lab1;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
 
 public class HelloMain {
+
     public static void main(String[] args) throws JMSException {
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
 
         /*
-        Create Connection instance from ConnectionFactory
+         Create Connection instance from ConnectionFactory
 
-        Create Session instance from connection object.
-        - session shouldn't by transactional and should by in auto acknowledge mode (Session.AUTO_ACKNOWLEDGE).
+         Create Session instance from connection object.
+         - session shouldn't by transactional and should by in auto acknowledge mode (Session.AUTO_ACKNOWLEDGE).
 
-        Create Destination queue from session (check Session class and createQueue method)
-        - queue name should be "SayHelloQueue"
+         Create Destination queue from session (check Session class and createQueue method)
+         - queue name should be "SayHelloQueue"
 
 
-        Create Destination topic instance from session (check Session class and createTopic method)
-        - topic name should be "SayHelloTopic"
+         Create Destination topic instance from session (check Session class and createTopic method)
+         - topic name should be "SayHelloTopic"
          */
-
-        Connection connection = null;
-        Session session = null;
-        Destination queue = null;
-        MessageConsumer consumer = null;
+        Connection connection = connectionFactory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination queue = session.createQueue("SayHelloQueue");
+        Topic topic = session.createTopic("SayHelloTopic");
+        MessageConsumer consumer = session.createConsumer(queue);
 
         /*
-        Create MessageConsumer instance from session (check Session class and createConsumer method)
+         Create MessageConsumer instance from session (check Session class and createConsumer method)
 
-        Implement onMessage in MessageListener interface
-        - check if message is in proper type (see message type in Say class) by instanceof
-        - get text from message (remember to cast message to proper type)
-        - print message text to sysout
-        - don't forget to handle JMSException
+         Implement onMessage in MessageListener interface
+         - check if message is in proper type (see message type in Say class) by instanceof
+         - get text from message (remember to cast message to proper type)
+         - print message text to sysout
+         - don't forget to handle JMSException
          */
         MessageListener helloListener = new MessageListener() {
             @Override
             public void onMessage(Message message) {
-                throw new UnsupportedOperationException();
+                if (message instanceof TextMessage) {
+                    try {
+                        String messageText = ((TextMessage) message).getText();
+                        System.out.println(messageText);
+                    } catch (JMSException ex) {
+                        Logger.getLogger(HelloMain.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             }
         };
 
         //Set MessageListener implementation as a message listener in MessageConsumer
-
+        consumer.setMessageListener(helloListener);
         connection.start();
     }
 }
